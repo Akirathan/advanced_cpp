db4a07e18495eb08c3596544c81cfdb45dd3951b
Pavel Marek <pavel.akira.marek@gmail.com>
2019-04-09 13:21:33 +0200

    CMake: Fix unit build

--- allocator/unit_tests.cmake
+++ allocator/unit_tests.cmake
@@ -6,6 +6,8 @@ include_directories(/usr/include/boost/test)
 
 add_executable(unit_tests
         tests/unit_tests.cpp
+        inblock_allocator.cpp
+        inblock_allocator.hpp
         )
 
 target_link_libraries(unit_tests ${Booost_UNIT_TEST_FRAMEWORK_LIBRARY})
