f4896ddaead62e639e6d3908110202664b72a05b
Pavel Marek <pavel.akira.marek@gmail.com>
2019-04-10 22:35:19 +0200

    LargeBin: Implement initialize_memory

--- allocator/large_bin.hpp
+++ allocator/large_bin.hpp
@@ -8,10 +8,15 @@
 #include <vector>
 #include <cmath>
 
+/**
+ * Acts as unsorted bin.
+ */
 class LargeBin {
 public:
-    /// Has to be multiple of alignment (8).
+    /// Has to be multiple of alignment (8). Also has to be higher than SmallBins::max_chunk_size_for_bins.
     static constexpr size_t min_chunk_size = 256;
+    /// Has to be multiple of alignment (8).
+    static constexpr size_t initial_gap_between_chunks_sizes = 8 * 6;
 
     /// This method has same semantics as SmallBins.initialize_memory.
     intptr_t initialize_memory(intptr_t start_addr, intptr_t end_addr)
@@ -19,7 +24,30 @@ public:
         assert(is_aligned(start_addr));
         assert(is_aligned(end_addr));
 
-        // TODO
+        intptr_t last_addr = start_addr;
+        size_t chunk_size = min_chunk_size;
+        while (fits_in_memory_region(start_addr, chunk_size, end_addr)) {
+            chunk_t *new_chunk = initialize_chunk(start_addr, chunk_size);
+            large_chunk_list.append_chunk(new_chunk);
+
+            chunk_size += initial_gap_between_chunks_sizes;
+            start_addr += get_chunk_size(new_chunk);
+            last_addr = start_addr;
+        }
+
+        return last_addr;
+    }
+
+    void store_chunk(chunk_t *chunk)
+    {
+        assert(chunk);
+
+        large_chunk_list.append_chunk(chunk);
+    }
+
+    chunk_t * pop_first_chunk()
+    {
+        return large_chunk_list.pop_first_chunk();
     }
 
     /**
@@ -29,12 +57,11 @@ public:
      */
     chunk_t * get_chunk_with_size_at_least(size_t payload_size)
     {
-        // TODO
-        return nullptr;
+        return large_chunk_list.get_chunk_with_size_at_least(payload_size);
     }
 
 private:
-    chunk_t *chunks;
+    ChunkList large_chunk_list;
 };
 
 
