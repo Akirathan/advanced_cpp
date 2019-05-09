23182624f3a5b4bb7b2a71f9577d2f887e679d4d
Pavel Marek <pavel.akira.marek@gmail.com>
2019-05-09 14:16:59 +0200

    Refactor for recodex

--- concurent_bitmap/concurrent_bitmap.h
+++ concurent_bitmap/concurrent_bitmap.h
@@ -1,25 +1,18 @@
 // concurrent_bitmap.h
 // Pavel Marek NPRG051 2018/2019
 
-//#define USE_BOOST
+// Note that the implementation is only in header, because there are some template inner classes.
 
 #include <cstdint>
 #include <cassert>
 #include <array>
 #include <atomic>
 #include <mutex>
-#include <sstream>
-
-#include "test_common.hpp" // TODO
-
-#ifdef USE_BOOST
-#include <boost/log/trivial.hpp>
-#endif
 
 /**
  * We need this function, because std::pow is not constexpr.
  */
-constexpr std::size_t const_pow(std::size_t base, std::size_t x) noexcept
+static constexpr std::size_t const_pow(std::size_t base, std::size_t x) noexcept
 {
     std::size_t res = base;
     while (x > 1) {
@@ -28,17 +21,6 @@ constexpr std::size_t const_pow(std::size_t base, std::size_t x) noexcept
     }
     return res;
 }
-
-// debug
-static std::string indentation(std::size_t indent_level)
-{
-    std::ostringstream output;
-    for (std::size_t i = 0; i < indent_level; ++i)
-        output << "  ";
-    return output.str();
-}
-
-
 
 class concurrent_bitmap {
 public:
@@ -59,69 +41,47 @@ public:
         m_root.set(key, value);
     }
 
-    std::size_t get_bytes_count() const
-    {
-        return m_root.get_set_bytes();
-    }
-
-    nodes_count get_nodes_count() const
-    {
-        return m_root.get_nodes_count(nodes_count{});
-    }
-
-    void log_bytes_count() const
-    {
-#ifdef USE_BOOST
-        BOOST_LOG_TRIVIAL(info) << "Total set bytes count = " << m_root.get_set_bytes();
-#endif
-    }
-
-    void log_nodes_count() const
-    {
-        auto nodes_count_ = m_root.get_nodes_count(nodes_count{});
-#ifdef USE_BOOST
-        BOOST_LOG_TRIVIAL(info) << "Nodes count:" << "\n\t Leaves count = " << nodes_count_.leaves_count
-                                                    << "\n\t Inner nodes count = " << nodes_count_.inner_nodes_count;
-#endif
-    }
-
 private:
-    static constexpr std::size_t l0_bits = 6;
-    static constexpr std::size_t l1_bits = 6;
-    static constexpr std::size_t l2_bits = 6;
+    // 32-bit key is divided into l0, l1, l2, and leaf blocks.
+    // Every block represents index into certain node.
+    // The indexing is done from most-significant bit (from left).
+    static constexpr std::size_t l0_block_bits = 6;
+    static constexpr std::size_t l1_block_bits = 6;
+    static constexpr std::size_t l2_block_bits = 6;
     static constexpr std::size_t leaf_block_bits = 11;
-    static_assert(l0_bits + l1_bits + l2_bits + leaf_block_bits == sizeof(key_type) * 8 - 3);
+    // We need 3 bits to index certain bit in a byte.
+    static_assert(l0_block_bits + l1_block_bits + l2_block_bits + leaf_block_bits == sizeof(key_type) * 8 - 3);
+
+    // Definitions of various bit ranges and node sizes.
     using bit_range_t = std::pair<std::size_t, std::size_t>;
     static constexpr bit_range_t leaf_bit_range = std::make_pair(3, 3 + leaf_block_bits);
-    static constexpr bit_range_t l2_bit_range = std::make_pair(leaf_bit_range.second, leaf_bit_range.second + l2_bits);
-    static constexpr bit_range_t l1_bit_range = std::make_pair(l2_bit_range.second, l2_bit_range.second + l1_bits);
-    static constexpr bit_range_t l0_bit_range = std::make_pair(l1_bit_range.second, l1_bit_range.second + l0_bits);
-    static constexpr std::size_t l0_array_size = const_pow(2, l0_bits);
-    static constexpr std::size_t l1_array_size = const_pow(2, l1_bits);
-    static constexpr std::size_t l2_array_size = const_pow(2, l2_bits);
-    static constexpr std::size_t leaf_block_array_size = const_pow(2, leaf_block_bits);
+    static constexpr bit_range_t l2_bit_range = std::make_pair(leaf_bit_range.second, leaf_bit_range.second + l2_block_bits);
+    static constexpr bit_range_t l1_bit_range = std::make_pair(l2_bit_range.second, l2_bit_range.second + l1_block_bits);
+    static constexpr bit_range_t l0_bit_range = std::make_pair(l1_bit_range.second, l1_bit_range.second + l0_block_bits);
+    static constexpr std::size_t l0_node_size = const_pow(2, l0_block_bits);
+    static constexpr std::size_t l1_node_size = const_pow(2, l1_block_bits);
+    static constexpr std::size_t l2_node_size = const_pow(2, l2_block_bits);
+    static constexpr std::size_t leaf_node_size = const_pow(2, leaf_block_bits);
 
-    class i_bitmap_node {
+    class i_node {
     public:
-        i_bitmap_node(std::size_t bit_idx_from, std::size_t bit_idx_to)
+        i_node(std::size_t bit_idx_from, std::size_t bit_idx_to)
             : m_bit_idx_from{static_cast<uint8_t>(bit_idx_from)},
             m_bit_idx_to{static_cast<uint8_t>(bit_idx_to)}
         {}
-        virtual ~i_bitmap_node() = default;
+        virtual ~i_node() = default;
         virtual void set(key_type key, value_type value) = 0;
         virtual value_type get(key_type key) const = 0;
-        virtual std::size_t get_set_bytes() const = 0;
-        virtual nodes_count get_nodes_count(nodes_count accumulator) const = 0;
 
     protected:
         const uint8_t m_bit_idx_from;
         const uint8_t m_bit_idx_to;
 
+        /// Deduces array index from key.
         std::size_t get_index_from_key(key_type key) const
         {
             key_type mask = static_cast<key_type>(1 << (m_bit_idx_to - m_bit_idx_from));
             mask--;
-
             key >>= m_bit_idx_from;
             key &= mask;
             return static_cast<std::size_t>(key);
@@ -129,10 +89,10 @@ private:
     };
 
     template <std::size_t array_size>
-    class bitmap_node : public i_bitmap_node {
+    class inner_node : public i_node {
     public:
-        bitmap_node(const concurrent_bitmap &bitmap, std::size_t bit_idx_from, std::size_t bit_idx_to)
-            : i_bitmap_node{bit_idx_from, bit_idx_to},
+        inner_node(const concurrent_bitmap &bitmap, std::size_t bit_idx_from, std::size_t bit_idx_to)
+            : i_node{bit_idx_from, bit_idx_to},
             m_bitmap{bitmap}
         {
             for (auto &&child : m_children) {
@@ -140,7 +100,7 @@ private:
             }
         }
 
-        ~bitmap_node() override
+        ~inner_node() override
         {
             for (auto &&child : m_children) {
                 if (child != nullptr) {
@@ -155,6 +115,7 @@ private:
             if (m_children[idx] == nullptr) {
                 std::lock_guard<std::mutex> lock{m_mtx};
                 if (m_children[idx] == nullptr) {
+                    // Create new child node.
                     auto [next_from_idx, next_to_idx] = m_bitmap.get_next_bit_indexes(m_bit_idx_from, m_bit_idx_to);
                     m_children[idx] = m_bitmap.create_bitmap_node(next_from_idx, next_to_idx);
                 }
@@ -173,39 +134,17 @@ private:
             }
         }
 
-        std::size_t get_set_bytes() const override
-        {
-            std::size_t set_bytes_count = 0;
-            for (i_bitmap_node *child : m_children) {
-                if (child != nullptr) {
-                    set_bytes_count += child->get_set_bytes();
-                }
-            }
-            return set_bytes_count;
-        }
-
-        nodes_count get_nodes_count(nodes_count accumulator) const override
-        {
-            accumulator.inner_nodes_count++;
-            for (const i_bitmap_node *child : m_children) {
-                if (child != nullptr) {
-                    accumulator = child->get_nodes_count(accumulator);
-                }
-            }
-            return accumulator;
-        }
-
     private:
         const concurrent_bitmap &m_bitmap;
         std::mutex m_mtx;
-        std::array<i_bitmap_node *, array_size> m_children;
+        std::array<i_node *, array_size> m_children;
     };
 
     template <std::size_t array_size>
-    class bitmap_leaf_node : public i_bitmap_node {
+    class leaf_node : public i_node {
     public:
-        bitmap_leaf_node(std::size_t bit_idx_from, std::size_t bit_idx_to)
-            : i_bitmap_node{bit_idx_from, bit_idx_to},
+        leaf_node(std::size_t bit_idx_from, std::size_t bit_idx_to)
+            : i_node{bit_idx_from, bit_idx_to},
             m_data{}
         {}
 
@@ -228,36 +167,25 @@ private:
             return (m_data[byte_idx] & (1 << bit_idx)) != 0;
         }
 
-        std::size_t get_set_bytes() const override
-        {
-            std::size_t set_bytes_count = 0;
-            for (const std::atomic<uint8_t> &data : m_data) {
-                if (data != 0)
-                    set_bytes_count++;
-            }
-            return set_bytes_count;
-        }
-
-        nodes_count get_nodes_count(nodes_count accumulator) const override
-        {
-            accumulator.leaves_count++;
-            return accumulator;
-        }
-
     private:
         static constexpr key_type bit_idx_mask = 0x000000007;
         std::array<std::atomic<uint8_t>, array_size> m_data;
 
+        /// Deduces index of bit from last 3 bits in key.
         std::size_t get_bit_index(key_type key) const
         {
             key &= bit_idx_mask;
-            assert(key >= 0 && key <= 7);
             return static_cast<std::size_t>(key);
         }
     };
 
-    bitmap_node<l0_array_size> m_root;
+    inner_node<l0_node_size> m_root;
 
+    /**
+     * Returns pair of bit indexes that follows given pair of indexes, eg. l1 follows l0, l2 follows l1, etc.
+     * @param bit_idx_from Current min bit index.
+     * @param bit_idx_to Current max bit index.
+     */
     std::pair<std::size_t, std::size_t> get_next_bit_indexes(std::size_t bit_idx_from, std::size_t bit_idx_to) const
     {
         assert(leaf_bit_range.first <= bit_idx_from && bit_idx_to <= l0_bit_range.second);
@@ -276,18 +204,23 @@ private:
         }
     }
 
-    i_bitmap_node * create_bitmap_node(std::size_t bit_idx_from, std::size_t bit_idx_to) const
+    /**
+     * Creates either inner node or leaf node depending on parameters.
+     * @param bit_idx_from Min bit index for new node.
+     * @param bit_idx_to Max bit index for new node.
+     */
+    i_node * create_bitmap_node(std::size_t bit_idx_from, std::size_t bit_idx_to) const
     {
         assert(leaf_bit_range.first <= bit_idx_from && bit_idx_to <= l1_bit_range.second);
 
         if (are_indexes_in_bitrange(l1_bit_range, bit_idx_from, bit_idx_to)) {
-            return new bitmap_node<l1_array_size>{*this, bit_idx_from, bit_idx_to};
+            return new inner_node<l1_node_size>{*this, bit_idx_from, bit_idx_to};
         }
         else if (are_indexes_in_bitrange(l2_bit_range, bit_idx_from, bit_idx_to)) {
-            return new bitmap_node<l2_array_size>{*this, bit_idx_from, bit_idx_to};
+            return new inner_node<l2_node_size>{*this, bit_idx_from, bit_idx_to};
         }
         else if (are_indexes_in_bitrange(leaf_bit_range, bit_idx_from, bit_idx_to)) {
-            return new bitmap_leaf_node<leaf_block_array_size>{bit_idx_from, bit_idx_to};
+            return new leaf_node<leaf_node_size>{bit_idx_from, bit_idx_to};
         }
         else {
             return nullptr;
